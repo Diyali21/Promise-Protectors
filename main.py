b2509fbcb78c7b1e6f76c31da67b53b098383a59
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-03-29 11:11:57 +0200

    home and venue details layout done

---
+++ main.py
@@ -0,0 +1,17 @@
+venue_details = [
+    {
+        "venue_name": "A secret garden, hidden just for you",
+        "venue_description": "Imagine a place, where time stands still, and all that exists is the beauty of nature and the love you share. In this picturesque garden venue, the beauty of nature sets the stage for a romantic and unforgettable wedding celebration. Spend your special day surrounded by the sights, sounds, and scents of nature, your love will flourish, and your hearts will forever be entwined.",
+        "max_people": 100,
+    },
+    {
+        "venue_name": "xx",
+        "venue_description": "xx",
+        "max_people": 100,
+    },
+    {
+        "venue_name": "xx",
+        "venue_description": "xx",
+        "max_people": 100,
+    },
+]
