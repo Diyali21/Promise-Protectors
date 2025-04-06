c05202293bca0bbbc800bef2a18063498036daef
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-04-06 17:04:23 +0200

    get covered form more refined

--- models/insurance_cover.py
+++ models/insurance_cover.py
@@ -6,6 +6,7 @@ class InsureCover(db.Model):
     cover_id = db.Column(db.Integer, primary_key=True)
     cover_name = db.Column(db.String(50), nullable=False)
     cover_price = db.Column(db.Float, nullable=False)
+    cover_description = db.Column(db.String(200), nullable=False)
 
     def to_dict(self):
         return {
